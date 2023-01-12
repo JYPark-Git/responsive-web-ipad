d0f6a9e4576b444ceca2d5a8b9c0849c6b06c42a
JYPark-Git <jypark.git@gmail.com>
2023-01-12 22:17:44 +0900

    add search area

--- js/main.js
+++ js/main.js
@@ -16,11 +16,24 @@ basketContent.addEventListener("click", e => {
 screen.addEventListener("click", e => {
   hideBasket();
 });
-
-
 function showBasket() {
   basketImg.classList.add("show");
 }
 function hideBasket() {
   basketImg.classList.remove("show");
 }
+
+const searchHeader = document.querySelector("header");
+const searchStartBtn = document.querySelector("li.search-starter");
+const searchCloseBtn = document.querySelector("div.search-closer");
+const shadowSection = document.querySelector("div.shadow");
+
+searchStartBtn.addEventListener("click", e => {
+  searchHeader.classList.add("searching");
+});
+searchCloseBtn.addEventListener("click", e => {
+  searchHeader.classList.remove("searching");
+});
+shadowSection.addEventListener("click", e => {
+  searchHeader.classList.remove("searching");
+});
